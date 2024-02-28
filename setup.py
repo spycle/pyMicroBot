cfc4e65ca3a7527aa59214f87ab9cba4a2d63b88
spycle <48740594+spycle@users.noreply.github.com>
2024-02-28 18:37:53 +0000

    Update setup.py
    Up-version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="PyMicroBot",
     packages=["microbot"],
     install_requires=["bleak>=0.19.0", "bleak-retry-connector>=1.4.0"],
-    version="0.0.20",
+    version="0.0.21",
     description="A library to communicate with MicroBot",
     author="spycle",
     url="https://github.com/spycle/pyMicroBot/",
